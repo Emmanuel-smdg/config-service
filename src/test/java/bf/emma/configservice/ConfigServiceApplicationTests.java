d4b3d99d8cf7c79d7dd02a67e2885039bb780f05
Emmanuel Samandoulougou <samabdoulougoue@gmail.com>
2023-02-13 10:23:54 +0000

    Enable Config service annotation and git config server uri

---
+++ src/test/java/bf/emma/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bf.emma.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
